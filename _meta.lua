09fdc8b3eee5b760dd07a69ce63e94aa8f39a7f8
Moritz John <moritz.john@posteo.com>
2025-12-22 08:02:59 +0100

    Change version 2.1.0

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = "homeassistant",
     fullname = _("Home Assistant"),
     description = _([[This plugin allows KOReader to control Home Assistant entities through its REST API.]]),
-    version = "v2.0.0",
+    version = "v2.1.0",
 }
