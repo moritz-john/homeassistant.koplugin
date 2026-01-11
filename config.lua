50b1d7aa609c3f0ce7cab09e30ae919d41ac2f3e
Moritz John <moritz.john@posteo.com>
2026-01-11 08:21:43 +0100

    Change config order; adjust comments

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 return {
     -- Home Assistant connection settings
     host = "192.168.1.10", -- Change to your Home Assistant IP Address or Hostname
-    https = false,         -- False or empty for http, true for https
-    port = 8123,           -- Default Home Assistant Port (probably 443 if using https)
+    port = 8123,           -- Home Assistant Port (usually 443 for HTTPS)
+    https = false,         -- Set true only if your Home Assistant is served over HTTPS
     token =                -- Change to your own Long-Lived Access Token
     "PasteYourHomeAssistantLong-LivedAccessTokenHere",
 
